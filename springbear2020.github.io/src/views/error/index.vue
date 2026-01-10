2e6c402b6b84823ea9fd446c8709a1b43966954b
leoli <leoli@yeahka.com>
2026-01-10 09:21:23 +0800

    feat: merge springbear2020.github.io into here

---
+++ springbear2020.github.io/src/views/error/index.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const handleClick = () => {
+  router.push("/");
+};
+</script>
+
+<template>
+  <div class="error-box">
+    <el-empty
+      description="页面被熊吃掉啦~"
+      image="/images/404.png"
+      :image-size="512"
+    >
+      <el-button round type="primary" icon="ArrowLeft" @click="handleClick"
+        >返回主页
+      </el-button>
+    </el-empty>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.error-box {
+  height: calc(100vh - 60px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
